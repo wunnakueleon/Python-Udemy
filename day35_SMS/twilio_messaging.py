3573500910fdce48c9df3ac8b31e827b31f3c9e6
Wunna <wannamoesan11@gmail.com>
2026-02-11 11:07:28 +0700

    SMS api sending

--- day35_SMS/twilio_messaging.py
+++ day35_SMS/twilio_messaging.py
@@ -1,3 +1,5 @@
+from twilio.rest import Client
+import time
 import requests
 import datetime
 from dotmap import DotMap
@@ -7,25 +9,38 @@ api_key = ""
 latitude = 16.8409
 longitude = 96.1735
 
+account_sid = ""
+auth_token = ""
+ph_num = ""
+my_num = ""
+
+
 data = requests.get(url=f"http://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={api_key}")
 # print(data.raise_for_status())
 data_json = DotMap(data.json())
 # print(data.json())
 
-hour_gaps = [3, 6, 9, 12, 15, 18, 21, 24]
+hour_gaps = [3, 6, 9, 10, 15, 18, 21, 24]
 day = 0
-current_hour = datetime.datetime.now().hour
 end_of_day = 0
 old_end_of_day = 0
 weather_condition = ""
  
 while True:
+    current_hour = datetime.datetime.now().hour
     if (current_hour in hour_gaps) :
         end_of_day = hour_gaps.index(current_hour) % len(hour_gaps) # because it starts from 0, it's not the exact length
 
         if (end_of_day != old_end_of_day):
             weather_condition = data_json.list[end_of_day].weather[0].description
             print(weather_condition)
+            client = Client(account_sid, auth_token)
+            message = client.messages .create (
+                        body=f"{weather_condition}: Do as you see fit!🤩",
+                        from_=ph_num,
+                        to=my_num
+                    )
+            print(message.body)
             if (end_of_day == len(hour_gaps) - 1):
                 day += 1
 
@@ -35,19 +50,12 @@ while True:
                 day = 0
                 
             old_end_of_day = end_of_day
+
+    time.sleep(60)
         
 
 
     
-
-
-
-
-
-
-
-
-
 
 
 
